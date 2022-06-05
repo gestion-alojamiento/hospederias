ed5bc73d12e3f7b8b7c385e6cb78f543e2534313
gestion-alojamiento <gesalojamiento@gmail.com>
2022-06-06 00:23:31 +0200

    Divide index.js en config.js

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-/*
- * CONFIGURACION SERVIDOR
- * 
+/**
+ * HERRAMIENTAS
  */
-
- /*
- * INFO ESTABLECIMIENTO
- */
- 
- const Establecimiento = {
-  alias: 'White Apartments & Sunrise Suites',
-  firebaseDOC: 'WhiteApartments',
-  huespedCOL: 'huesped',
-}
-// ---------------------------------------
-
-
- // Define la ruta de inicio para el 'router'
- const rutaInicio = '/'
- 
- 
-/*
- * GESTIÓN USUARIOS
- */
- 
-  const adminUsersOBJ = {
-	info: {
-		UID: 'Kd1ZOVY0NNRntrCVlD7LnXr4kbG2',
-		correo: 'info@whiteapartments.com'
-	},
-	gesalojamiento: {
-		UID: 'fbOd44zBIZNGg80nuwkIlIK4tqO2',
-		correo: 'gesalojamiento@gmail.com'
-	}
-  }
-
-// Selecciona cual de los usuarios tiene funciones de administrador
-  const adminUserUID = adminUsersOBJ.info.UID // Administrador => info
-
-// ------------------------------------------------------
-
-
 
 /*
  * ORDEN DE OBJETOS
  */
  
- function compareValues(key, order = 'asc') {
+function compareValues(key, order = 'asc') {
   return function innerSort(a, b) {
     if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
       // property doesn't exist on either object
@@ -68,47 +29,6 @@
     );
   };
 }
-
-      
-      	  
-/**
- * FECHAS
- * Genera una nueva clase Fecha() declarada en clases.js
- */
-
- const fecha = new Fecha(new Date())
- const hoy = fecha.hoy
- const ayer = fecha.ayer
-
-
-/*
- * HOSPEDERÍAS
- */
- 
-
-// Valores de inicio para el formulario ------------------
-const valorDeInicio = {
-	sexo: 'F',
-	nacionalidad: 'ESPAÑA',
-	tipoDocumento: 'D',
-	provincia: null
-}
-// -----------------------------
-
-// Valores por defecto en caso de error u omisión --------------
-const valorPorDefecto = {
-	numIdentificacion: '00000000',
-	fNacimiento: '2000-01-01',
-	fEntrada: fecha.aInput(ayer),
-	fExpedicionDoc: '2021-01-01'
-}
-// ---------------------------------
-
-
-/**
- * HERRAMIENTAS
- */
-
 
 // CADENAS  -------------------------------------------------------------
 
@@ -212,24 +132,7 @@ function accentFold(inStr) {
     "IRLANDA",
     "RUMANIA"
   ]
-  // Se muestra solo a españoles
-  const documentosObjES = {
-    "D": "DNI",
-    "P": "PASAPORTE",
-    "C": "PERMISO CONDUCIR ESPAÑOL"
-  }
-  // Se muestra sólo a nacionalizados dentro de la UE
-  const documentosObjEU = {
-    "P": "PASAPORTE",
-    "I": "DOCUMENTO DE IDENTIDAD DE LA UE",
-    "N": "NIE O TARJETA ESPAÑOLA DE EXTRANJEROS"
-  }
-  // Se muestra a todos menos a los españoles
-  const documentosObjOtros = {
-    "P": "PASAPORTE",
-    "N": "NIE O TARJETA ESPAÑOLA DE EXTRANJEROS",
-    "X": "PERMISO DE RESIDENCIA DE ESTADO MIEMBRO DE LA UE"
-  }
+  
   const regionesES = [
 	"Andalucía",
 	"Aragón",
