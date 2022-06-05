70ca0782756510270f31870e0022cbfc20f70137
gestion-alojamiento <gesalojamiento@gmail.com>
2022-06-05 23:57:13 +0200

    Nuevo estilo en menuHuesped

--- componente/menuHuesped.js
+++ componente/menuHuesped.js
@@ -1,10 +1,11 @@
 const menuHuesped = {
   template: `
 	<ul class="navbar-horizontal">
-		<li><router-link to="/huesped/alta"> ➕ Alta huésped</router-link></li>
+		<li><router-link :to="{ name: 'huespedAlta' }"> ➕ Alta huésped</router-link></li>
 		<li><router-link to="/huesped/lista"> 🔍 Buscar huésped</router-link></li>
 		<li><router-link to="/huesped/hospederias"> 🛂 Hospederías</router-link></li>
 		<li><router-link :to="{ name: 'huespedLeeme' }"> 📑 Léeme</router-link></li>
+		<li style="float:right"><a class="active" href="#about">Inicio</a></li>
 		<div hidden>➕©👁‍🗨🛂🔍🔎📫✏✒🖋🖊🖌🖍👮‍♀ 👨🕹📚📃📄✍  ‍💻️ </div>
 	</ul>
 	`
