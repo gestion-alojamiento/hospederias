0424af58e5b36285dc427f5c58a6e144d4e75d30
gestion-alojamiento <gesalojamiento@gmail.com>
2022-06-05 23:54:53 +0200

    separar código en config.js

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
-/*
- * CONFIGURACION SERVIDOR
- * 
+/**
+ * FECHAS
+ * Genera una nueva clase Fecha() declarada en clases.js
  */
 
+ const fecha = new Fecha(new Date())
+ const hoy = fecha.hoy
+ const ayer = fecha.ayer
+ 
  /*
- * INFO ESTABLECIMIENTO
- */
- 
- const Establecimiento = {
-  alias: 'White Apartments & Sunrise Suites',
-  firebaseDOC: 'WhiteApartments',
-  huespedCOL: 'huesped',
-}
-// ---------------------------------------
-
-
- // Define la ruta de inicio para el 'router'
- const rutaInicio = '/'
- 
- 
-/*
- * GESTIÓN USUARIOS
- */
- 
-  const adminUsersOBJ = {
-	info: {
-		UID: 'Kd1ZOVY0NNRntrCVlD7LnXr4kbG2',
-		correo: 'info@whiteapartments.com'
-	},
-	gesalojamiento: {
-		UID: 'fbOd44zBIZNGg80nuwkIlIK4tqO2',
-		correo: 'gesalojamiento@gmail.com'
-	}
-  }
-
-// Selecciona cual de los usuarios tiene funciones de administrador
-  const adminUserUID = adminUsersOBJ.info.UID // Administrador => info
-
-// ------------------------------------------------------
-
-
-
-/*
  * ORDEN DE OBJETOS
  */
  
@@ -68,47 +34,6 @@
     );
   };
 }
-
-      
-      	  
-/**
- * FECHAS
- * Genera una nueva clase Fecha() declarada en clases.js
- */
-
- const fecha = new Fecha(new Date())
- const hoy = fecha.hoy
- const ayer = fecha.ayer
-
-
-/*
- * HOSPEDERÍAS
- */
- 
-
-// Valores de inicio para el formulario ------------------
-const valorDeInicio = {
-	sexo: 'F',
-	nacionalidad: 'ESPAÑA',
-	tipoDocumento: 'D',
-	provincia: null
-}
-// -----------------------------
-
-// Valores por defecto en caso de error u omisión --------------
-const valorPorDefecto = {
-	numIdentificacion: '00000000',
-	fNacimiento: '2000-01-01',
-	fEntrada: fecha.aInput(ayer),
-	fExpedicionDoc: '2021-01-01'
-}
-// ---------------------------------
-
-
-/**
- * HERRAMIENTAS
- */
-
 
 // CADENAS  -------------------------------------------------------------
 
